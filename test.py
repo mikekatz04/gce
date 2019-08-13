31aaceada2563c91d9abb068bf72792f326c0625
Michael Katz <mikekatz04@gmail.com>
2019-08-13 11:34:16 -0500

    test updated for katie data set

--- test.py
+++ test.py
@@ -5,7 +5,13 @@ import time
 from astropy.io import ascii
 
 from ztfperiodic import simulate
-from gcex.gce import ConditionalEntropy
+
+try:
+    from gcex.gce import ConditionalEntropy
+except ImportError:
+    print('GCE not found.')
+
+M_sun = 1.989e30
 
 def py_check_ce(freqs, time_vals, magnitude_vals, mag_bins=10, phase_bins=15, verbose=False):
     ce_vals = np.zeros_like(freqs)
@@ -36,7 +42,7 @@ def py_check_ce(freqs, time_vals, magnitude_vals, mag_bins=10, phase_bins=15, ve
 
 def read_helper(fp):
     # add new routines as necessary
-    if fp[-4:] == '.txt':
+    if fp[-4:] == '.txt' or fp[-4:] == '.csv':
         data = np.asarray(ascii.read(fp))
         keys = data.dtype.names
 
@@ -46,7 +52,29 @@ def read_helper(fp):
         m2 = data['m2']
         params['q'] = m1/m2 * (m1 >= m2) + m2/m1 * (m1 < m2)
 
+    return params
 
+def cosmic_read_helper(fp, x_sun=0.0, y_sun=0.0, z_sun=0.0):
+    data = np.asarray(ascii.read(fp))
+    keys = data.dtype.names
+
+    params = {key: data[key] for key in keys}
+    params['m1'] = m1 = params['m1 [msun]']
+    params['m2'] = m2 = params['m2[msun]']
+    params['q'] = q = m1/m2 * (m1 >= m2) + m2/m1 * (m1 < m2)
+
+    params['d'] = 1e3*((params['xGx[kpc]'] - x_sun)**2 + (params['yGx[kpc]'] - y_sun)**2 + (params['zGx[kpc]'] - z_sun)**2)**(1/2)
+    P0_sec = 2./params['f_gw[Hz]']
+    params['period'] = P0_sec/(3600.0*24.0)
+    params['a'] = a = (P0_sec**2/(4*ct.pi**2) * ct.G * (m1 + m2)*M_sun)**(1/3) / 695700e3  # solar radii
+
+    params['radius_1'] = 1.5e-2*(0.5/m1)  # 4.168e-3 * (1/m1)**(1/3) / a
+    params['radius_2'] = 1.5e-2*(0.5/m2)  # 4.168e-3 * (1/m2)**(1/3) / a
+    fdot_gw = params['f_dot_total [yr^(-2)]'] / ct.Julian_year**2
+    params['Pdot'] = -1./2. * fdot_gw*P0_sec**2
+
+    params['incl'] = np.ones_like(params['Pdot']) * 100.0
+    params['sbratio'] = np.ones_like(params['Pdot']) * 0.5
     return params
 
 
@@ -71,12 +99,13 @@ def test(input_dict):
 
     lcs = []
     ce_checks = []
-    number_of_pts = np.random.random_integers(80, 97, size=num_lcs)
+    number_of_pts = np.random.random_integers(95, 105, size=num_lcs)
     for lc_i, n in zip(np.arange(num_lcs), number_of_pts):
         # form dictionary
         params = {key: input_dict[key][lc_i] for key in keys}
 
-        t_obs = simulate.time(n=n, mean_dt=3, sig_t=2)
+        t_obs = simulate.time(n=n, mean_dt=3, sig_t=3)
+
         mag, phase, err = simulate.pdot_lc(t_obs, plot_nopdot=None, **params)
         """mag=None, absmag=True, d=None, Pdot=Pdot, radius_1=r1/a, radius_2=r2/a, sbratio=sbratio, incl=i,
            light_3 = 0, t_zero = 0, period = P0, a = a, q = m1/m2,
@@ -98,10 +127,13 @@ def test(input_dict):
            exact_grav=False, verbose=1, plot_nopdot=True,savefig=False)"""
 
         lcs.append(np.array([t_obs, mag]).T)
-
+        if len(np.where(mag != 1.0)[0]) != 0:
+            print('CHECKER')
+        if lc_i % 10 == 0:
+            print(lc_i)
         #check = py_check_ce(test_freqs, time_vals, mags, mag_bins=10, phase_bins=15, verbose=verbose)
         #ce_checks.append(check)
-
+    import pdb; pdb.set_trace()
     #pyce_checks = np.asarray(ce_checks)
     ce = ConditionalEntropy()
     batch_size = 200
@@ -111,7 +143,7 @@ def test(input_dict):
     check = ce.batched_run_const_nfreq(lcs, batch_size, test_freqs, test_pdots, show_progress=True)
     et = time.perf_counter()
     print('Time per frequency per pdot per light curve:', (et - st)/(num_lcs*num_freqs*num_pdots))
-    print('Total time for {} light curves and {} frequencies and {} pdots per lc:'.format(num_lcs, num_freqs, num_pdots), et - st)
+    print('Total time for {} light curves and {} frequencies and {} pdots:'.format(num_lcs, num_freqs, num_pdots), et - st)
     #checker = actual_freqs/test_freqs[np.argmin(check, axis=1)]
 
     #sig = (np.min(check, axis=1) - np.mean(check, axis=1))/np.std(check, axis=1)
@@ -122,5 +154,7 @@ def test(input_dict):
 
 
 if __name__ == "__main__":
-    input_dict = read_helper('test_params.txt')
+    #input_dict = read_helper('test_params.txt')
+    input_dict = cosmic_read_helper('gx_save_lambda_var_alpha_025.csv', x_sun=0.0, y_sun=0.0, z_sun=0.0)
+    print('Read data complete.')
     test(input_dict)

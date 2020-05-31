00fd497e0462ed901e479244b269ec150652c767
mikekatz04 <mikekatz04@gmail.com>
2020-05-31 14:24:44 -0500

    kernel.cu documenbted

--- gcex/src/kernel.cu
+++ gcex/src/kernel.cu
@@ -20,8 +20,11 @@
 #include <random>
 #include "global.h"
 
+
 #define NUM_THREADS 64
 
+
+// function for error checking
 #define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
 inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
 {
@@ -32,6 +35,8 @@ inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=t
    }
 }
 
+
+// Custom mod function
 __device__
 double mod_func(double x, double y)
 {
@@ -43,62 +48,48 @@ double mod_func(double x, double y)
 }
 
 
+// Phase fold of time and pick phase bins
 __device__
 int get_phase_bin(fod t_val, fod pdot, fod frequency, fod period, fod half_dbins, int phase_bins){
     double folded_val = mod_func((double) t_val-0.5*pdot*frequency*(t_val*t_val), (double)period)*((double)frequency); // between 0 and 1
+
+    // revert negative fold values
     if (folded_val < 0.0) folded_val *= -1.0;
 
+    // find phase bin
     int j = (int) abs((folded_val/(double)half_dbins));
-    //printf("1: %d %d %lf %lf\n", j, phase_bins, folded_val, half_dbins);
     j = j % phase_bins;
-    //else if (j>phase_bins)  j = phase_bins;
-    //printf("2: %d %d %lf %lf\n", j, phase_bins, folded_val, half_dbins);
+
     return j;
 }
 
+
 __device__ fod ce (fod frequency, fod pdot,
                    int* __restrict__ mag_bin_inds, fod* __restrict__ time_vals, int npoints,
-                   int mag_bins, int phase_bins,
-                   int offset, int lc_i,
+                   int mag_bins, int phase_bins, int lc_i,
                    fod * temp_phase_prob, fod *overall_phase_prob, fod half_dbins){
+
     fod period = 1./frequency;
     fod folded_val = 0.0;
-    int mag_ind_1 = -1, mag_ind_2=-1;
-    int l = 1;
+
     fod sum_ij = 0.0;
     fod t_val = 0.0;
     fod total_points = 0.0;
     int j = 0;
     int j1 = 0;
 
+    // clear phase values
     for (int jj = 0; jj<phase_bins; jj+=1){
             overall_phase_prob[jj] = 0.0;
             temp_phase_prob[jj] = 0.0;
         }
 
-    //for (int i=0; i<mag_bins; i++){
-    //    mag_bin_vals[i] = 0;
-    //}
-
-    //fod current_phase_prob = 0.0;
-    int temp_mag_points = 0;
-    short index=0;
     int ind_mag=0;
-
     int temp_ind = 0;
     int gap = 0;
+
+    // get population of overall phase bins
     for (int k=0; k<npoints; k++){
-
-        /*# if __CUDA_ARCH__>=200
-        if ((offset == 0)  && (lc_i == 0)){
-            printf("(%d %d %d): %lf - %lf\n", lc_i, j, offset, phase_bin_edges[2*j], phase_bin_edges[2*j+1]);
-        }
-        #endif //*/
-        //int k = 0;
-        //while(k < npoints - gap){
-
-            ///index = indicies[k];
-            //if (index == -1) break;
 
             t_val = time_vals[k];
             j = get_phase_bin(t_val, pdot, frequency, period, half_dbins, phase_bins);
@@ -106,13 +97,15 @@ __device__ fod ce (fod frequency, fod pdot,
             overall_phase_prob[j] += 1.0;
             total_points += 1.0;
 
-            //if (k == mag_bins) printf("%lf %lf %lf %lf, %d %d\n", t_val, pdot, period, half_dbins, j, phase_bins);
-
+            // account for 50% overlap
             int j1 = (j <= 0) ? j = phase_bins - 1 : (j - 1) % phase_bins;
             overall_phase_prob[j1] += 1.0;
             total_points += 1.0;
 
         }
+
+    // now find population of each individual bin
+    // operations are done within a 1d histogam of phase bins (single mag)
 
     ind_mag = mag_bin_inds[0];
     int kk = 0;
@@ -121,8 +114,9 @@ __device__ fod ce (fod frequency, fod pdot,
             t_val = time_vals[kk];
             j = get_phase_bin(t_val, pdot, frequency, period, half_dbins, phase_bins);
 
+            // fill singular bin
+
             temp_phase_prob[j] += 1.0;
-            //printf("%lf %lf %lf %lf, %d %d %d\n", t_val, pdot, period, half_dbins, j, mag_bin_inds[kk], ind_mag);
             int j1 = (j <= 0) ? j = phase_bins - 1 : (j - 1) % phase_bins ;
             temp_phase_prob[j1] += 1.0;
 
@@ -130,73 +124,31 @@ __device__ fod ce (fod frequency, fod pdot,
             if (kk == npoints) break;
         }
 
+        // sum all bins at this magnitude
         for (int jj = 0; jj<phase_bins; jj+=1){
             if ((overall_phase_prob[jj] > 0.0) && (temp_phase_prob[jj] > 0.0)){
-                 //printf("%d %d %lf %lf \n", ind_mag, jj, overall_phase_prob[jj], temp_phase_prob[jj]);
                 sum_ij += temp_phase_prob[jj]*log(overall_phase_prob[jj]/temp_phase_prob[jj]);
                 temp_phase_prob[jj] = 0.0;
             }
         }
         if (kk < npoints) ind_mag = mag_bin_inds[kk];
     }
-                //printf("%d, %lf, %lf\n", l, mag_bins[l-1], current_phase_prob);
 
-            //        indicies[k] = indicies[(npoints - 1) - gap];
-            //        gap += 1;
-            //}
-            //else k++;
-        //}
-        /*# if __CUDA_ARCH__>=200
-        if ((offset == mag_bins*255)  && (lc_i == 0)){
-            printf("(%d %d %d %.18e %e)\n", lc_i, j, offset, frequency, current_phase_prob);
-            for (int jj=0; jj<mag_bins; jj++) printf("%e, ", mag_bin_vals[offset + jj]);
-            printf("\n");
-            for (int jj=0; jj<mag_bins+1; jj++) printf("%e, ", mag_bin_edges[jj]);
-            printf("\n\n");
-        }
-        #endif //*/
-
-
-    //for (int j=0; j<phase_bins*mag_bins; j++){
-        //if (current_phase_prob[j] == 0.0) continue;
-        //fod curr_phase_p = current_phase_prob[j];
-        //for (int i=0; i<mag_bins; i++){
-            //if (bin_counter[j*mag_bins + i] > 0.0){
-        //        continue;
-                //if (j < 2) printf("%d, %d, %lf, %lf\n", j, i,bin_counter[j*mag_bins + i], curr_phase_p);
-            //    sum_ij += (bin_counter[j*mag_bins + i])*log((curr_phase_p)/(bin_counter[j*mag_bins + i]));
-        //    }
-
-        //}
-    /*# if __CUDA_ARCH__>=200
-    if ((offset == mag_bins*255)  && (lc_i == 0)){
-        printf("(%d %d %.18e CE: %.18e)\n", lc_i, offset, frequency, sum_ij);
-    }
-    #endif //*/
-    //if (lc_i == 1) printf("%lf %lf %d\n", sum_ij, total_points, npoints);
     return  sum_ij/((fod) total_points);
-    //return 0.0;
 }
 
+
+// Function wrap for conditional entropy
 __global__ void kernel(fod* __restrict__ ce_vals, fod* __restrict__ freqs, int num_freqs, fod* __restrict__ pdots,
                        int num_pdots, int* __restrict__ mag_bin_inds,
                        fod* __restrict__ time_vals, int * __restrict__ num_pts_arr, int num_pts_max,
                        const int mag_bins, int phase_bins, int num_lcs, fod half_dbins){
 
-
-    // __shared__ fod share_mag_bin_vals[NUM_THREADS*mag_bins];
+    // declare and assign dynamic shared memory
     extern __shared__ fod time_vals_share[];
     int *mag_bin_inds_share = (int*)(&time_vals_share[num_pts_max]);
-    //printf("%ld %ld, %ld\n", time_vals_share, mag_bin_inds_share, num_pts_max*2*sizeof(fod));
     fod * overall_phase_prob = (fod*) &time_vals_share[2*num_pts_max];
-    //__shared__ fod temp_phase_prob[phase_bin*NUM_THREADS];
     fod *temp_phase_prob = (fod*) &time_vals_share[2*num_pts_max + phase_bins*NUM_THREADS];
-
-
-    //for (int j=threadIdx.x; j<=phase_bins; j+=blockDim.x){
-    //    share_bins_phase[2*j] = phase_bin_edges[2*j];
-    //    share_bins_phase[2*j+1] = phase_bin_edges[2*j+1];
-  // }
 
     __syncthreads();
 
@@ -207,17 +159,13 @@ __global__ void kernel(fod* __restrict__ ce_vals, fod* __restrict__ freqs, int n
 
      int num_pts_this_lc = num_pts_arr[lc_i];
 
-     //printf("%d\n", num_pts_this_lc);
-
+     // use coalesced memory transfers to load time and magnitude values
      for (int j=threadIdx.x; j<num_pts_this_lc; j+=blockDim.x){
 
          time_vals_share[j] = time_vals[lc_i*num_pts_max + j];
          mag_bin_inds_share[j] = mag_bin_inds[lc_i*num_pts_max + j];
 
-         //printf("%d\n", mag_bin_inds_share[j]);
      }
-
-      //printf("CHECK\n");
 
      __syncthreads();
 
@@ -229,59 +177,47 @@ __global__ void kernel(fod* __restrict__ ce_vals, fod* __restrict__ freqs, int n
          i < num_freqs;
          i += blockDim.x * gridDim.x) {
 
-    // TODO: make this adjustable: https://devblogs.nvidia.com/using-shared-memory-cuda-cc/  !!!!
-    int offset = mag_bins*threadIdx.x;
-
-
-    //__shared__ fod share_t_vals[mag_bins0];
-    //__shared__ fod share_magnitude[mag_bins0];
-
-        //for (int j=0; j<num_pts_this_lc; j++) share_t_vals[j] = time_vals[lc_i*num_pts_max + j];
-        //for (int j=0; j<num_pts_this_lc; j++) share_magnitude[j] = mag_vals[lc_i*num_pts_max + j];
-        /*# if __CUDA_ARCH__>=200
-        if ((freqs[i] < 2.314814814814814839e-07) && (lc_i == 1)){
-            printf("%.18e\n", freqs[i]);
-            for (int jj=0; jj<=mag_bins; jj++) printf("%e, ", share_bins_mag[jj]);
-            printf("\n\n");
-        }
-
-
-        #endif //*/
-
+    // run ce computation for this set of parameters
     ce_vals[(lc_i*num_pdots + pdot_i)*num_freqs + i] = ce(freqs[i], pdots[pdot_i], mag_bin_inds_share,
                                                           &time_vals_share[0], num_pts_this_lc, mag_bins, phase_bins,
-                                                          offset, lc_i,
+                                                          lc_i,
                                                           &temp_phase_prob[threadIdx.x*phase_bins], &overall_phase_prob[threadIdx.x*phase_bins], half_dbins);
   }
 }
 }
 }
 
+
+// wrapper function callable from another script or cython
 void run_gce(fod *d_ce_vals, fod *d_freqs, int num_freqs, fod *d_pdots, int num_pdots, int *d_mag_bin_inds, fod *d_time_vals,
              int *d_num_pts_arr, int num_pts_max, int mag_bins, int phase_bins, int num_lcs, fod half_dbins)
 {
+
+    // number of blocks given the thread num setting (64)
     int nblocks = (int)ceil((num_freqs + NUM_THREADS - 1)/NUM_THREADS);
-    //printf("%d\n", nblocks, num_pdots, num_lcs);
     dim3 griddim(nblocks, num_lcs, num_pdots);
 
-    //cudaStream_t streams[num_lcs];
+    // determine shared memory allocation size
+    size_t numBytes = sizeof(fod)*num_pts_max
+                      + sizeof(int)*num_pts_max
+                      + sizeof(fod)*phase_bins*NUM_THREADS
+                      + sizeof(fod)*phase_bins*NUM_THREADS;
 
-    //for (int lc_i=0; lc_i<num_lcs; lc_i+=1){
-        //cudaStreamCreate(&streams[lc_i]);
+    kernel<<<griddim, NUM_THREADS, numBytes>>>(d_ce_vals,
+                                               d_freqs, num_freqs,
+                                               d_pdots,
+                                               num_pdots,
+                                               d_mag_bin_inds,
+                                               d_time_vals,
+                                               d_num_pts_arr,
+                                               num_pts_max,
+                                               mag_bins,
+                                               phase_bins,
+                                               num_lcs,
+                                               half_dbins);
 
-        size_t numBytes = sizeof(fod)*num_pts_max + sizeof(int)*num_pts_max + sizeof(fod)*phase_bins*NUM_THREADS + sizeof(fod)*phase_bins*NUM_THREADS;
-        //printf("num_bytes: %d, %d, %d\n", numBytes, sizeof(int), num_pts_max*2*sizeof(fod));
-        kernel<<<griddim, NUM_THREADS, numBytes>>>(d_ce_vals, d_freqs, num_freqs, d_pdots, num_pdots, d_mag_bin_inds, d_time_vals,
-                                     d_num_pts_arr, num_pts_max, mag_bins, phase_bins,
-                                     num_lcs, half_dbins);
-    //}
     cudaDeviceSynchronize();
     gpuErrchk(cudaGetLastError());
-
-    //for (int lc_i=0; lc_i<num_lcs; lc_i+=1){
-    //        cudaStreamDestroy(streams[lc_i]);
-
-    //    }
 
 }
 
@@ -291,21 +227,15 @@ __global__ void kernel_share(fod* __restrict__ ce_vals, fod* __restrict__ freqs,
                        fod* __restrict__ time_vals, int * __restrict__ num_pts_arr, int num_pts_max,
                        const int mag_bins, int phase_bins, int num_lcs, fod half_dbins){
 
-       // __shared__ fod share_mag_bin_vals[NUM_THREADS*10];
-//       extern __shared__ fod share_bins_phase[];
+       // prepare dynamically allocated shared memory
        extern __shared__ fod overall_phase_prob[];
        fod *bin_counts = &overall_phase_prob[phase_bins];
 
+       // shared quantities for atomicAdd
        __shared__ fod total_points;
        __shared__ fod sum_ij;
 
        __shared__ fod period, freq, pdot;
-
-       //for (int j=threadIdx.x; j<=phase_bins; j+=blockDim.x){
-       //    share_bins_phase[2*j] = phase_bin_edges[2*j];
-       //    share_bins_phase[2*j+1] = phase_bin_edges[2*j+1];
-     // }
-
 
        __syncthreads();
 
@@ -326,6 +256,8 @@ __global__ void kernel_share(fod* __restrict__ ce_vals, fod* __restrict__ freqs,
        for (int i = blockIdx.x;
             i < num_freqs;
             i += gridDim.x) {
+
+        // initialize all quantities
 
         if (threadIdx.x == 0){
         pdot = pdots[pdot_i];
@@ -350,9 +282,11 @@ __global__ void kernel_share(fod* __restrict__ ce_vals, fod* __restrict__ freqs,
 
         for (int jj=threadIdx.x; jj<num_pts_this_lc; jj+=blockDim.x){
 
+            // get phase bin
             int j = get_phase_bin(time_vals[lc_i*num_pts_max + jj], pdot, freq, period, half_dbins, phase_bins);
             int mag_ind = mag_bin_inds[lc_i*num_pts_max + jj];
 
+            // fill histogram
             atomicAdd(&overall_phase_prob[j], 1.0);
             atomicAdd(&bin_counts[j*mag_bins + mag_ind], 1.0);
             atomicAdd(&total_points, 1.0);
@@ -362,6 +296,7 @@ __global__ void kernel_share(fod* __restrict__ ce_vals, fod* __restrict__ freqs,
             atomicAdd(&bin_counts[j2*mag_bins + mag_ind], 1.0);
             atomicAdd(&total_points, 1.0);
 
+            // check if there should be magnitude overlap
             if (mag_ind != 0 && mag_ind != mag_bins - 1){
 
                 mag_ind -= 1;
@@ -374,25 +309,19 @@ __global__ void kernel_share(fod* __restrict__ ce_vals, fod* __restrict__ freqs,
                 atomicAdd(&bin_counts[j2*mag_bins + mag_ind], 1.0);
                 atomicAdd(&total_points, 1.0);
             }
-
-            //if (i == 950) printf("%d %d %d %d %d %d, %lf %lf\n",lc_i, pdot_i, i, jj, j, mag_ind, overall_phase_prob[j], bin_counts[j*mag_bins + mag_ind]);
-
         }
 
         __syncthreads();
 
-        //fod sum_ij = 0.0;
+        // SUM all bins in the 2D histogram
         for (int jj=threadIdx.x; jj<phase_bins*mag_bins; jj+=blockDim.x){
+            // this will create nans if bin_counts[jj] is 0.0
             if (bin_counts[jj] == 0.0) continue;
             int j = jj/mag_bins;
 
-            //printf("%d %d, %lf %lf\n", jj, j, overall_phase_prob[j], bin_counts[jj]);
-            //if (overall_phase_prob[j] <bin_counts[jj]) printf("bad %d %d %d %d %d, %lf %lf\n",lc_i, pdot_i, i, jj, j, overall_phase_prob[j], bin_counts[jj]);
             atomicAdd(&sum_ij, bin_counts[jj]*log(overall_phase_prob[j]/bin_counts[jj]));
         }
         __syncthreads();
-        //if (i == 0) printf("%lf %lf\n", sum_ij, total_points);
-
         ce_vals[(lc_i*num_pdots + pdot_i)*num_freqs + i] = sum_ij/total_points;
 
      }
@@ -401,31 +330,28 @@ __global__ void kernel_share(fod* __restrict__ ce_vals, fod* __restrict__ freqs,
    }
 
 
+// Wrapper function for the long gce version
+//callable from another script or cython
    void run_long_lc_gce(fod *d_ce_vals, fod *d_freqs, int num_freqs, fod *d_pdots, int num_pdots, int *d_mag_bin_inds, fod *d_time_vals,
                 int *d_num_pts_arr, int num_pts_max, int mag_bins, int phase_bins, int num_lcs, fod half_dbins)
 {
-    int nblocks = (int)ceil((num_freqs + NUM_THREADS - 1)/NUM_THREADS);
-    //printf("%d\n", nblocks, num_pdots, num_lcs);
-    //dim3 griddim(nblocks, num_lcs, num_pdots);
+
+    // Set grid: each freq and pdot are given singular block,
+    // rather than thread
     dim3 griddim(num_freqs, num_lcs, num_pdots);
 
-    int nthreads_long = 256;
+    // number o
+    int NUM_THREADS_LONG = 256;
+
+    // get size of shared memory
     size_t numBytes = phase_bins*sizeof(fod) + mag_bins*phase_bins*sizeof(fod);
-    kernel_share<<<griddim, nthreads_long, numBytes>>>(d_ce_vals, d_freqs, num_freqs, d_pdots, num_pdots,
+
+    kernel_share<<<griddim, NUM_THREADS_LONG, numBytes>>>(d_ce_vals, d_freqs, num_freqs, d_pdots, num_pdots,
                                  d_mag_bin_inds, d_time_vals,
                                  d_num_pts_arr, num_pts_max, mag_bins, phase_bins,
                                  num_lcs, half_dbins);
-    //cudaStream_t streams[num_lcs];
 
-    //for (int lc_i=0; lc_i<num_lcs; lc_i+=1){
-        //cudaStreamCreate(&streams[lc_i]);
-    //}
     cudaDeviceSynchronize();
     gpuErrchk(cudaGetLastError());
-
-    //for (int lc_i=0; lc_i<num_lcs; lc_i+=1){
-    //        cudaStreamDestroy(streams[lc_i]);
-
-    //    }
 
 }

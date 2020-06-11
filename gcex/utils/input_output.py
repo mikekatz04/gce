b8e1edfda45709eea7be9f2324e625e1129d88fd
mikekatz04 <mikekatz04@gmail.com>
2020-06-11 17:14:39 -0500

    updates

--- gcex/utils/input_output.py
+++ gcex/utils/input_output.py
@@ -1,13 +1,26 @@
-import h5py
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (C) Michael Katz (2020)
+#
+# This file is part of gce
+#
+# gce is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# gce is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with gce.  If not, see <http://www.gnu.org/licenses/>
+
+
 import numpy as np
 import scipy.constants as ct
-from astropy.io import ascii
-import os
 import pickle
-import pandas as pd
-
-Msun = 1.989e30
-rsun = 6.957 * (10 ** 5)
 
 
 def read_in_for_paper(
